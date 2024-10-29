6678c68ee6d673100153d665b6959ccbdadbe3f6
WillGuerreiros <williamnunes182@gmail.com>
2024-10-29 16:15:30 -0300

    first commit

---
+++ frontend-hackathon/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import { AndroidFilled } from '@ant-design/icons'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <AndroidFilled />
+  )
+}
+
+export default App
